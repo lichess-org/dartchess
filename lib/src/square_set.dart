ed25eebb9dfa052950ea2a13c6189961efabaeb9
Vincent Velociter <vincent.velociter@gmail.com>
2022-07-21 10:14:58 +0200

    Add more methods to SquareSet

--- lib/src/square_set.dart
+++ lib/src/square_set.dart
@@ -1,5 +1,18 @@
 /// A set of squares represented by a 64 bit integer mask, using little endian
 /// rank-file (LERF) mapping.
+/// This is how it looks like:
+///
+///  8 | 56 57 58 59 60 61 62 63
+///  7 | 48 49 50 51 52 53 54 55
+///  6 | 40 41 42 43 44 45 46 47
+///  5 | 32 33 34 35 36 37 38 39
+///  4 | 24 25 26 27 28 29 30 31
+///  3 | 16 17 18 19 20 21 22 23
+///  2 | 8  9  10 11 12 13 14 15
+///  1 | 0  1  2  3  4  5  6  7
+///    -------------------------
+///      a  b  c  d  e  f  g  h
+///
 class SquareSet {
   const SquareSet(this.value);
   const SquareSet.fromSquare(int square)
@@ -31,6 +44,22 @@ class SquareSet {
     return this;
   }
 
+  SquareSet complement() {
+    return SquareSet(~value);
+  }
+
+  SquareSet xor(SquareSet other) {
+    return SquareSet(value ^ other.value);
+  }
+
+  SquareSet union(SquareSet other) {
+    return SquareSet(value | other.value);
+  }
+
+  SquareSet intersect(SquareSet other) {
+    return SquareSet(value & other.value);
+  }
+
   int size() {
     return _popcnt64(value);
   }
@@ -41,6 +70,18 @@ class SquareSet {
 
   SquareSet withSquare(int square) {
     return SquareSet(value | (1 << square));
+  }
+
+  String debugPrint() {
+    final r = [];
+    for (int y = 7; y >= 0; y--) {
+      for (int x = 0; x < 8; x++) {
+        final square = x + y * 8;
+        r.add(has(square) ? '1' : '.');
+        r.add(x < 7 ? ' ' : '\n');
+      }
+    }
+    return r.join('');
   }
 
   @override
