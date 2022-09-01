6b2cf9d67d62c0fb2d0656459b085639969199ed
Vincent Velociter <vincent.velociter@gmail.com>
2022-09-01 10:25:33 +0200

    Add more code examples

--- example/dartchess_example.dart
+++ example/dartchess_example.dart
@@ -1,5 +1,21 @@
 import 'package:dartchess/dartchess.dart';
 
 void main() {
-  print(SquareSet.full);
+  // Parse fen and create a valid chess position
+  final setup = Setup.parseFen(
+      'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1');
+  final pos = Chess.fromSetup(setup);
+
+  // Generate legal moves
+  final legal = pos.legalMoves;
+  assert(legal.length == 20);
+
+  // Play moves
+  final pos2 = pos.play(Move(from: 12, to: 28));
+
+  // Detect game end conditions
+  assert(pos2.isGameOver == false);
+  assert(pos2.isCheckmate == false);
+  assert(pos2.outcome == null);
+  assert(pos2.isInsufficientMaterial == false);
 }
