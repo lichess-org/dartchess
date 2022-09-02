3d6c311493a4220da1bfe30c9e2cc86338fec688
Vincent Velociter <vincent.velociter@gmail.com>
2022-09-02 17:45:26 +0200

    Fix format

--- example/perft.dart
+++ example/perft.dart
@@ -4,5 +4,6 @@ void main() {
   final stopwatch = Stopwatch()..start();
   final depth = 4;
   perft(Chess.initial, depth);
-  print('initial position perft at depht $depth executed in ${stopwatch.elapsed.inMilliseconds} ms');
+  print(
+      'initial position perft at depht $depth executed in ${stopwatch.elapsed.inMilliseconds} ms');
 }
