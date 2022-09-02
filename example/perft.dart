6388ae2bb590fd8c1bf439a965db7a30c800ca40
Vincent Velociter <vincent.velociter@gmail.com>
2022-09-02 16:53:25 +0200

    Add a perft example

---
+++ example/perft.dart
@@ -0,0 +1,8 @@
+import 'package:dartchess/dartchess.dart';
+
+void main() {
+  final stopwatch = Stopwatch()..start();
+  final depth = 4;
+  perft(Chess.initial, depth);
+  print('initial position perft at depht $depth executed in ${stopwatch.elapsed.inMilliseconds} ms');
+}
