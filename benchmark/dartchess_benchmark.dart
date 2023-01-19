41624cf81514caa40d8c731b5f07c95b983b4506
Vincent Velociter <vincent.velociter@gmail.com>
2023-01-19 11:48:12 +0100

    Add a parse san benchmark

--- benchmark/dartchess_benchmark.dart
+++ benchmark/dartchess_benchmark.dart
@@ -14,6 +14,15 @@ void main() {
     perft(Chess.initial, 4);
   }, iterations: 1);
 
+  benchmark('parse san moves', () {
+    const moves =
+        'e4 Nc6 Bc4 e6 a3 g6 Nf3 Bg7 c3 Nge7 d3 O-O Be3 Na5 Ba2 b6 Qd2 Bb7 Bh6 d5 e5 d4 Bxg7 Kxg7 Qf4 Bxf3 Qxf3 dxc3 Nxc3 Nac6 Qf6+ Kg8 Rd1 Nd4 O-O c5 Ne4 Nef5 Rd2 Qxf6 Nxf6+ Kg7 Re1 h5 h3 Rad8 b4 Nh4 Re3 Nhf5 Re1 a5 bxc5 bxc5 Bc4 Ra8 Rb1 Nh4 Rdb2 Nc6 Rb7 Nxe5 Bxe6 Kxf6 Bd5 Nf5 R7b6+ Kg7 Bxa8 Rxa8 R6b3 Nd4 Rb7 Nxd3 Rd1 Ne2+ Kh2 Ndf4 Rdd7 Rf8 Ra7 c4 Rxa5 c3 Rc5 Ne6 Rc4 Ra8 a4 Rb8 a5 Rb2 a6 c2';
+    Position pos = Chess.initial;
+    for (final san in moves.split(' ')) {
+      pos = pos.play(pos.parseSan(san)!);
+    }
+  }, iterations: 1);
+
   benchmark('valid fen moves', () {
     const fen = 'rn1qkb1r/pbp2ppp/1p2p3/3n4/8/2N2NP1/PP1PPPBP/R1BQ1RK1 b kq -';
     final pos = Chess.fromSetup(Setup.parseFen(fen));
